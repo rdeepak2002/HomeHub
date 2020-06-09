9d20242973af1b0cd81aa83dd0bfcccd3890baf8
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-09 15:46:34 -0700

    added react

---
+++ src/Main.js
@@ -0,0 +1,36 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+const fullscreen = false
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 1024, height: 600, fullscreen: fullscreen})
+
+  mainWindow.loadURL('http://localhost:3000')
+
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
