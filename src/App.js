9c212b59e555b4d42ad80c2e4e3a00c413d8547d
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-09 18:19:13 -0700

    added single page functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Route, NavLink, HashRouter } from 'react-router-dom'
+
+import HomeScreen from './HomeScreen'
+import SettingsScreen from './SettingsScreen'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <HashRouter>
+      <div>
+        <h1>Simple SPA</h1>
+        <ul className="header">
+          <li><a href="/#/">HomeScreen</a></li>
+          <li><a href="/#/settings">Settings</a></li>
+        </ul>
+        <div className="content">
+          <Route exact path="/" component={HomeScreen}/>
+          <Route path="/settings" component={SettingsScreen}/>
+        </div>
+      </div>
+    </HashRouter>
   )
 }
 
-export default App;
+export default App
