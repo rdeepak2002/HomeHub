2e4347af792e07c1107003a662df6d1105b1fa05
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-09 19:28:39 -0700

    added settings glyph

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,11 +1,17 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
+import '../Styles/HomeScreen.css'
+import settingsGlyph from '../Images/settings-solid.svg'
 
 class HomeScreen extends Component {
   render() {
     return (
       <div>
-        <h2>HOME SCREEN</h2>
-        <a href="/#/settings">settings</a>
+        <a class="settingsBtn" href="/#/settings">
+          <img width="100px" height="100px" src={settingsGlyph}></img>
+        </a>
+        <div class="cameraContainer">
+
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './Styles/index.css';
-import App from './Screens/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './Styles/index.css'
+import App from './Screens/App'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
