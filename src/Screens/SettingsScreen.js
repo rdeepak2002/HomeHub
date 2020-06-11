40463ceb96ed82682f7a25caca1225d04fac27e3
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-11 11:52:26 -0700

    added shutdown command

--- src/Screens/SettingsScreen.js
+++ src/Screens/SettingsScreen.js
@@ -29,6 +29,32 @@ class SettingsScreen extends Component {
     }
   }
 
+  shutdown = () => {
+    var data = {}
+
+    data.command = 'sudo shutdown -h now'
+
+    var xmlhttp = new XMLHttpRequest()
+    let theUrl = 'http://localhost:8080/command'
+
+    xmlhttp.open('POST', theUrl)
+    xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+    xmlhttp.send(JSON.stringify(data))
+
+    xmlhttp.onload  = function (e) {
+      if (xmlhttp.readyState === 4) {
+        if (xmlhttp.status === 200) {
+          let obj = JSON.parse(xmlhttp.responseText);
+          alert(obj.message)
+        } else {
+          console.error(xmlhttp.statusText)
+          console.log(2)
+          alert('Error contacting server.')
+        }
+      }
+    }
+  }
+
   render() {
     return (
       <div className="screen">
@@ -38,6 +64,7 @@ class SettingsScreen extends Component {
         </a>
         <div className="btnContainer">
           <button onClick={this.reboot}>Reboot</button>
+          <button onClick={this.shutdown}>Shutdown</button>
         </div>
       </div>
     )
