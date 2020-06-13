2d90b2a064ad893ca4ebd541327aaeb9de6efddb
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-12 20:05:08 -0700

    hopefully things are fixed now

--- src/electron-start.js
+++ src/electron-start.js
@@ -5,7 +5,7 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
-const fullscreen = true
+const fullscreen = false
 const dev = false
 
 let mainWindow
